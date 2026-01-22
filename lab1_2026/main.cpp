dae5881c8f9bd770c36aa75e5c50ce217a3d5850
Jaden Prante <jaden.prante@loki>
2026-01-21 23:09:30 -0800

    This is the start of Lab 3

--- lab1_2026/main.cpp
+++ lab1_2026/main.cpp
@@ -22,162 +22,107 @@
 cSymbolTable g_symbolTable;
 long long cSymbol::nextId = 0;
 yylval_t yylval;
-int g_insert = 1;           // insert symbols into the table
-int g_local = 0;            // local lookups only
+int g_insert = 1;           // global to indicate that symbols should be 
+                            // inserted into the symbol table
+int g_local = 0;            // global to indicate to do local lookups
 
-// Process identifiers and insert/find in symbol table
-static int ProcessID()
+// **************************************************
+// argv[1] is the input file
+// argv[2] if present, is the output file
+int main(int argc, char **argv)
 {
-    std::string name(yytext);
-    cSymbol* sym; 
-    if (g_insert)
-    {
-        sym = g_symbolTable.FindLocal(name);
-        if (!sym)
-        {
-            sym = new cSymbol(name);
-            g_symbolTable.Insert(sym);
-        }
-    }
-    else
-    {
-        sym = new cSymbol(name);
-    }
-    yylval.symbol = sym;
-    return IDENTIFIER;
-}
-
-int main(int argc, char** argv)
-{
-    const char* outfile_name;
+    const char *outfile_name;
+    int result = 0;
     int token;
 
-    std::cout << "Jaden Prante" << std::endl;
+    std::cout << "Philip Howard" << std::endl;
 
-    // open input file
+    // open input
     if (argc > 1)
     {
         yyin = fopen(argv[1], "r");
-        if (!yyin)
+        if (yyin == NULL)
         {
-            std::cerr << "Unable to open input file: " << argv[1] << "\n";
+            std::cerr << "Unable to open file " << argv[1] << "\n";
             exit(-1);
         }
     }
 
-    // redirect output if requested
+    // open output
     if (argc > 2)
     {
         outfile_name = argv[2];
-        FILE* output = fopen(outfile_name, "w");
-        if (!output)
+        FILE *output = fopen(outfile_name, "w");
+        if (output == NULL)
         {
-            std::cerr << "Unable to open output file: " << outfile_name << "\n";
+            std::cerr << "Unable to open output file " << outfile_name << "\n";
             exit(-1);
         }
         int output_fd = fileno(output);
         if (dup2(output_fd, 1) != 1)
         {
-            std::cerr << "Unable to redirect output to " << outfile_name << "\n";
+            std::cerr << "Unable to send output to " << outfile_name << "\n";
             exit(-2);
         }
     }
 
-    // TEST2 flag
-    bool do_test2 = (argc > 3);
-
-    if (!do_test2) { std::cout << "<program>\n"; }
+    std::cout << "<program>\n";
 
     token = yylex();
     while (token != 0)
     {
-        if (do_test2)
+        // if we found an identifier, print it out
+        if (token == IDENTIFIER) 
         {
-            // TEST2 output
-            if (token == IDENTIFIER)
-                printf("%d:%s:%lld\n", token, yytext, yylval.symbol->GetId());
-            else
-                printf("%d:%s\n", token, yytext);
-        }
-        else
-        {
-            cSymbol* sym = nullptr;
-
-            switch (token)
+            cSymbol *sym;
+            if (!g_insert)
             {
-            case IDENTIFIER:
-                sym = yylval.symbol;
-                if (!g_insert)
-                {
-                    if (g_local)
-                        sym = g_symbolTable.FindLocal(sym->GetName());
-                    else
-                        sym = g_symbolTable.Find(sym->GetName());
-                    if (!sym) break; // don't print unknown temp symbols
-                    yylval.symbol = sym;
-                }
-                std::cout << "<sym id=\"" << sym->GetId() << "\" name=\"" << sym->GetName() << "\"/>\n";
-                break;
+                if (g_local)
+                    sym = g_symbolTable.FindLocal(yylval.symbol->GetName());
+                else
+                    sym = g_symbolTable.Find(yylval.symbol->GetName());
 
-            case GLOBAL:
-                sym = g_symbolTable.FindLocal("GLOBAL");
-                if (!sym)
-                {
-                    sym = new cSymbol("GLOBAL");
-                    g_symbolTable.Insert(sym);
-                }
-                std::cout << "<sym id=\"" << sym->GetId() << "\" name=\"" << sym->GetName() << "\"/>\n";
-                g_local = 0;
-                break;
-
-            case INSERT:
-                sym = g_symbolTable.FindLocal("INSERT");
-                if (!sym)
-                {
-                    sym = new cSymbol("INSERT");
-                    g_symbolTable.Insert(sym);
-                }
-                std::cout << "<sym id=\"" << sym->GetId() << "\" name=\"" << sym->GetName() << "\"/>\n";
-                g_insert = 1;
-                break;
-
-            case LOOKUP:
-                sym = g_symbolTable.FindLocal("LOOKUP");
-                if (!sym)
-                {
-                    sym = new cSymbol("LOOKUP");
-                    g_symbolTable.Insert(sym);
-                }
-                std::cout << "<sym id=\"" << sym->GetId() << "\" name=\"" << sym->GetName() << "\"/>\n";
-                g_insert = 0;
-                break;
-
-            case LOCAL:
-                sym = g_symbolTable.FindLocal("LOCAL");
-                if (!sym)
-                {
-                    sym = new cSymbol("LOCAL");
-                    g_symbolTable.Insert(sym);
-                }
-                std::cout << "<sym id=\"" << sym->GetId() << "\" name=\"" << sym->GetName() << "\"/>\n";
-                g_local = 1;
-                break;
-
-            case OPEN:
-                std::cout << "<open/>\n";
-                g_symbolTable.IncreaseScope();
-                break;
-
-            case CLOSE:
-                std::cout << "<close/>\n";
-                g_symbolTable.DecreaseScope();
-                break;
-            default:
-                break;
+                if (sym != nullptr) yylval.symbol = sym;
             }
+
+            // this will either be the one found above or the one created
+            // in the scanner.
+            std::cout << yylval.symbol->ToString() << "\n";
+        }
+        else if (token == LOCAL)
+        {
+            std::cout << "<local />\n";
+            g_local = 1;
+        }
+        else if (token == GLOBAL)
+        {
+            std::cout << "<global />\n";
+            g_local = 0;
+        }
+        else if (token == LOOKUP)
+        {
+            std::cout << "<lookup />\n";
+            g_insert = 0;
+        }
+        else if (token == INSERT)
+        {
+            std::cout << "<insert />\n";
+            g_insert = 1;
+        }
+        else if (token == OPEN)
+        {
+            std::cout << "<open />\n";
+            g_symbolTable.IncreaseScope();
+        }
+        else if (token == CLOSE)
+        {
+            std::cout << "<close />\n";
+            g_symbolTable.DecreaseScope();
         }
         token = yylex();
     }
-    if (!do_test2) { std::cout << "</program>\n"; }
-    return 0;
+
+    std::cout << "</program>\n";
+
+    return result;
 }
